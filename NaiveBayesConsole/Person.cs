3c8a28f2355b9d16038332f63fd0778bcfc63c75
ijrussell <i.j.russell@tesco.net>
2018-01-30 17:58:09 +0000

    Converted array to list<person>

---
+++ NaiveBayesConsole/Person.cs
@@ -0,0 +1,10 @@
+﻿namespace NaiveBayesConsole
+{
+    public class Person
+    {
+        public string Role { get; set; }
+        public string Gender { get; set; }
+        public string Income { get; set; }
+        public string Voted { get; set; }
+    }
+}
--- NaiveBayesConsole/Program.cs
+++ NaiveBayesConsole/Program.cs
@@ -1,45 +1,56 @@
-﻿namespace NaiveBayesConsole
+﻿using System;
+using System.Collections.Generic;
+
+namespace NaiveBayesConsole
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var data = GetRawData();
+
+            // Split data into 24 items to train on 
+            // and 6 items to test your algorithm.
+
+            Console.WriteLine(data.Count);
+            Console.ReadLine();
         }
 
-        private static string[][] GetRawData()
+        private static List<Person> GetRawData()
         {
-            var rawData = new string[30][];
-            rawData[0] = new string[] { "analyst", "male", "high", "conservative" };
-            rawData[1] = new string[] { "barista", "female", "low", "liberal" };
-            rawData[2] = new string[] { "cook", "male", "medium", "conservative" };
-            rawData[3] = new string[] { "doctor", "female", "medium", "conservative" };
-            rawData[4] = new string[] { "analyst", "female", "low", "liberal" };
-            rawData[5] = new string[] { "doctor", "male", "medium", "conservative" };
-            rawData[6] = new string[] { "analyst", "male", "medium", "conservative" };
-            rawData[7] = new string[] { "cook", "female", "low", "liberal" };
-            rawData[8] = new string[] { "doctor", "female", "medium", "liberal" };
-            rawData[9] = new string[] { "cook", "female", "low", "liberal" };
-            rawData[10] = new string[] { "doctor", "male", "medium", "conservative" };
-            rawData[11] = new string[] { "cook", "female", "high", "liberal" };
-            rawData[12] = new string[] { "barista", "female", "medium", "liberal" };
-            rawData[13] = new string[] { "analyst", "male", "low", "liberal" };
-            rawData[14] = new string[] { "doctor", "female", "high", "conservative" };
-            rawData[15] = new string[] { "barista", "female", "medium", "conservative" };
-            rawData[16] = new string[] { "doctor", "male", "medium", "conservative" };
-            rawData[17] = new string[] { "barista", "male", "high", "conservative" };
-            rawData[18] = new string[] { "doctor", "female", "medium", "liberal" };
-            rawData[19] = new string[] { "analyst", "male", "low", "liberal" };
-            rawData[20] = new string[] { "doctor", "male", "medium", "conservative" };
-            rawData[21] = new string[] { "cook", "male", "medium", "conservative" };
-            rawData[22] = new string[] { "doctor", "female", "high", "conservative" };
-            rawData[23] = new string[] { "analyst", "male", "high", "conservative" };
-            rawData[24] = new string[] { "barista", "female", "medium", "liberal" };
-            rawData[25] = new string[] { "doctor", "male", "medium", "conservative" };
-            rawData[26] = new string[] { "analyst", "female", "medium", "conservative" };
-            rawData[27] = new string[] { "analyst", "male", "medium", "conservative" };
-            rawData[28] = new string[] { "doctor", "female", "medium", "liberal" };
-            rawData[29] = new string[] { "barista", "male", "medium", "conservative" };
-            return rawData;
+            return new List<Person>
+            {
+                new Person { Role = "analyst", Gender = "male", Income = "high", Voted = "conservative" },
+                new Person { Role = "barista", Gender = "female", Income = "low", Voted = "liberal" },
+                new Person { Role = "cook", Gender = "male", Income = "medium", Voted = "conservative" },
+                new Person { Role = "doctor", Gender = "female", Income = "medium", Voted = "conservative" },
+                new Person { Role = "analyst", Gender = "female", Income = "low", Voted = "liberal" },
+                new Person { Role = "doctor", Gender = "male", Income = "medium", Voted = "conservative" },
+                new Person { Role = "analyst", Gender = "male", Income = "medium", Voted = "conservative" },
+                new Person { Role = "cook", Gender = "female", Income = "low", Voted = "liberal" },
+                new Person { Role = "doctor", Gender = "female", Income = "medium", Voted = "liberal" },
+                new Person { Role = "cook", Gender = "female", Income = "low", Voted = "liberal" },
+                new Person { Role = "doctor", Gender = "male", Income = "medium", Voted = "conservative" },
+                new Person { Role = "cook", Gender = "female", Income = "high", Voted = "liberal" },
+                new Person { Role = "barista", Gender = "female", Income = "medium", Voted = "liberal" },
+                new Person { Role = "analyst", Gender = "male", Income = "low", Voted = "liberal" },
+                new Person { Role = "doctor", Gender = "female", Income = "high", Voted = "conservative" },
+                new Person { Role = "barista", Gender = "female", Income = "medium", Voted = "conservative" },
+                new Person { Role = "doctor", Gender = "male", Income = "medium", Voted = "conservative" },
+                new Person { Role = "barista", Gender = "male", Income = "high", Voted = "conservative" },
+                new Person { Role = "doctor", Gender = "female", Income = "medium", Voted = "liberal" },
+                new Person { Role = "analyst", Gender = "male", Income = "low", Voted = "liberal" },
+                new Person { Role = "doctor", Gender = "male", Income = "medium", Voted = "conservative" },
+                new Person { Role = "cook", Gender = "male", Income = "medium", Voted = "conservative" },
+                new Person { Role = "doctor", Gender = "female", Income = "high", Voted = "conservative" },
+                new Person { Role = "analyst", Gender = "male", Income = "high", Voted = "conservative" },
+                new Person { Role = "barista", Gender = "female", Income = "medium", Voted = "liberal" },
+                new Person { Role = "doctor", Gender = "male", Income = "medium", Voted = "conservative" },
+                new Person { Role = "analyst", Gender = "female", Income = "medium", Voted = "conservative" },
+                new Person { Role = "analyst", Gender = "male", Income = "medium", Voted = "conservative" },
+                new Person { Role = "doctor", Gender = "female", Income = "medium", Voted = "liberal" },
+                new Person { Role = "barista", Gender = "male", Income = "medium", Voted = "conservative" },
+            };
         }
     }
 }
